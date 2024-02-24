75893ed6f62b413e4a80ba1e5c21dce522135b20
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-02-24 07:07:46 -0700

    Add kGZIID_cIGZPersistDBSegment

--- allGZIDs.h
+++ allGZIDs.h
@@ -726,7 +726,8 @@ enum GZMSGID
 	kGZIID_cGZWinCtrlMgr = 0x22C2EB1F,
 	kGZIID_cS3DCamera = 0xE9C6262A,
 	kGZIID_cGZWinOptGrp = 0xA1336CC0,
-	kGZIID_cSC43DRender = 0x681bd529,
+	kGZIID_cSC43DRender = 0x681BD529,
+	kGZIID_cIGZPersistDBSegment = 0x656B8F0C.
 //interface id end
 
 // budget group start
